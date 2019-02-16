18357caad465e0a0bd27168569baeac05cf276d5
Christopher Peterson <codewarrior3@users.noreply.github.com>
2019-02-15 23:49:06 -0800

    Added SQL connection for Windows Authentication SQL DB

--- Application/Home.aspx.cs
+++ Application/Home.aspx.cs
@@ -9,15 +9,14 @@ namespace Application
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            /* string connstring = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
-
-            using (SqlConnection conn = new SqlConnection(connstring))
+             string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+             DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                using (SqlCommand cmdd = new SqlCommand("select * from [myDbTable]", conn))
+                using (SqlCommand cmdd = new SqlCommand("select * from [PersonAndTheirHobby]", conn))
                 {
                     using (SqlDataAdapter adapter = new SqlDataAdapter(cmdd))
                     {
-                        DataTable dt = new DataTable();
                         adapter.Fill(dt);
 
                         //Bind the datasource with a repeater control in which you can  place textbox control. It will repeat for every data rows.
@@ -26,7 +25,7 @@ namespace Application
                     }
                 }
 
-            }*/
+            }
         }
     }
 }
