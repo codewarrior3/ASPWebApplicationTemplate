6f6da3e389073235f054fe49c8d53efeb31c79a0
Christopher Peterson <codewarrior3@users.noreply.github.com>
2019-02-16 00:41:32 -0800

    Added Dropdown

--- Application/Home.aspx
+++ Application/Home.aspx
@@ -4,4 +4,5 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1>Home</h1>
+    <asp:DropDownList ID="ddlHobbies" Visible="True" DataTextField="Hobby" DataValueField="ID" runat="server"></asp:DropDownList>
 </asp:Content>
--- Application/Home.aspx.cs
+++ Application/Home.aspx.cs
@@ -13,16 +13,17 @@ namespace Application
             DataTable dt = new DataTable();
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                using (SqlCommand cmdd = new SqlCommand("select * from [PersonAndTheirHobby]", conn))
+                using (SqlCommand cmdd = new SqlCommand("select * from [Hobbies]", conn))
                 {
                     using (SqlDataAdapter adapter = new SqlDataAdapter(cmdd))
                     {
                         adapter.Fill(dt);
-
-                        //Bind the datasource with a repeater control in which you can  place textbox control. It will repeat for every data rows.
                     }
                 }
             }
+
+            ddlHobbies.DataSource = dt;
+            ddlHobbies.DataBind();
         }
     }
 }
