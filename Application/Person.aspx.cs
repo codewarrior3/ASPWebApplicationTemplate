f30638292fd1dd74e39ae1592ff8fe1402a94040
Christopher Peterson <codewarrior3@users.noreply.github.com>
2019-02-18 13:35:58 -0800

    Adding previous and Next Links

--- Application/Person.aspx.cs
+++ Application/Person.aspx.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Runtime.Serialization;
 
 namespace Application
 {
@@ -10,24 +11,39 @@ namespace Application
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
-                SetLabels();
+            {
+                DataTable dtPerson = new DataTable();
+                SetLabels(ref dtPerson);
+            }
         }
 
-        protected void SetLabels(Int32 rowId = 1)
+        protected DataTable GetPetsonByID(Int32 rowId)
         {
+            DataTable dt = new DataTable();
             var connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
-            var dt = new DataTable();
+
             using (var conn = new SqlConnection(connectionString))
             {
-                using (var cmdd = new SqlCommand("SELECT Person.ID, Name, DOB, Age, Hobbies.Hobby FROM Person, Hobbies where dbo.Person.Hobby = dbo.Hobbies.ID and Person.ID=" + rowId.ToString(), conn))
+                using (var cmd =
+                    new SqlCommand(
+                        "SELECT Person.ID, Name, DOB, Age, Hobbies.Hobby FROM Person, Hobbies where dbo.Person.Hobby = dbo.Hobbies.ID and Person.ID=" +
+                        rowId.ToString(), conn))
                 {
-                    using (var adapter = new SqlDataAdapter(cmdd))
+                    using (var adapter = new SqlDataAdapter(cmd))
                     {
                         adapter.Fill(dt);
                     }
                 }
             }
 
+            return dt;
+        }
+
+        //0 for none
+        //1 for add
+        //2 for subtract
+        protected void SetLabels(ref DataTable dt)
+        {
             if (dt.Rows.Count > 0)
             {
                 lblID.Text = dt.Rows[0]["ID"].ToString().Trim();
@@ -38,7 +54,7 @@ namespace Application
             }
         }
 
-        protected void lnkPrev_OnClick(object sender, EventArgs e)
+        protected void Previous()
         {
             if (!String.IsNullOrEmpty(lblID.Text))
             {
@@ -52,12 +68,14 @@ namespace Application
                 {
                     rowId = 2000;
                 }
-
-                SetLabels(rowId);
+                lblID.Text = rowId.ToString();
+                GetPetsonByID(rowId);
+                DataTable dtPerson = GetPetsonByID(rowId);
+                SetLabels(ref dtPerson);
             }
         }
 
-        protected void lnkNext_OnClick(object sender, EventArgs e)
+        protected void Next()
         {
             int rowId;
             var parseId = Int32.TryParse(lblID.Text, out rowId);
@@ -69,7 +87,20 @@ namespace Application
             {
                 rowId = 1;
             }
-            SetLabels(rowId);
+
+            lblID.Text = rowId.ToString();
+            DataTable dtPerson = GetPetsonByID(rowId);
+            SetLabels(ref dtPerson);
+        }
+
+        protected void lnkPrev_OnClick(object sender, EventArgs e)
+        {
+            Previous();
+        }
+
+        protected void lnkNext_OnClick(object sender, EventArgs e)
+        {
+            Next();
         }
     }
 }
