fc9278008c3afc2e793f736fda0c430978a3a621
Christopher Peterson <codewarrior3@users.noreply.github.com>
2019-02-18 00:00:36 -0800

    Added Next and Previous

---
+++ Application/Person.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Person.aspx.cs" Inherits="Application.Person" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>People</h1>
+    <asp:LinkButton ID="lnkPrev" OnClick="lnkPrev_OnClick" runat="server">Previous</asp:LinkButton>
+    <asp:LinkButton ID="lnkNext" OnClick="lnkNext_OnClick" runat="server">Next</asp:LinkButton>
+    <br />
+    <br />
+    ID:
+    <asp:Label ID="lblID" runat="server" Text="1"></asp:Label>
+    <br />
+    Name:
+    <asp:Label ID="lblName" runat="server" Text=""></asp:Label>
+    <br />
+    Age:
+    <asp:Label ID="lblAge" runat="server" Text=""></asp:Label>
+    <br />
+    Date of Birth:
+    <asp:Label ID="lblDob" runat="server" Text=""></asp:Label>
+    <br />
+    Hobby:
+    <asp:Label ID="lblHobby" runat="server" Text=""></asp:Label>
+</asp:Content>
---
+++ Application/Person.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Application
+{
+    public partial class Person : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                SetLabels();
+        }
+
+        protected void SetLabels(Int32 rowId = 1)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+            var dt = new DataTable();
+            using (var conn = new SqlConnection(connectionString))
+            {
+                using (var cmdd = new SqlCommand("SELECT Person.ID, Name, DOB, Age, Hobbies.Hobby FROM Person, Hobbies where dbo.Person.Hobby = dbo.Hobbies.ID and Person.ID=" + rowId.ToString(), conn))
+                {
+                    using (var adapter = new SqlDataAdapter(cmdd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+
+            if (dt.Rows.Count > 0)
+            {
+                lblID.Text = dt.Rows[0]["ID"].ToString().Trim();
+                lblAge.Text = dt.Rows[0]["Age"].ToString().Trim();
+                lblName.Text = dt.Rows[0]["Name"].ToString().Trim();
+                lblDob.Text = Convert.ToDateTime(dt.Rows[0]["DOB"].ToString().Trim()).ToShortDateString();
+                lblHobby.Text = dt.Rows[0]["Hobby"].ToString().Trim();
+            }
+        }
+
+        protected void lnkPrev_OnClick(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(lblID.Text))
+            {
+                int rowId;
+                var parseId = Int32.TryParse(lblID.Text, out rowId);
+                if (parseId && rowId > 1)
+                {
+                    rowId -= 1;
+                }
+                else
+                {
+                    rowId = 2000;
+                }
+
+                SetLabels(rowId);
+            }
+        }
+
+        protected void lnkNext_OnClick(object sender, EventArgs e)
+        {
+            int rowId;
+            var parseId = Int32.TryParse(lblID.Text, out rowId);
+            if (parseId && rowId < 2000)
+            {
+                rowId += 1;
+            }
+            else
+            {
+                rowId = 1;
+            }
+            SetLabels(rowId);
+        }
+    }
+}
